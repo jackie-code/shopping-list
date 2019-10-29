07a083a5b2930a93db7dc8a10a01c76977463c25
Jacqueline Martin <52892367+jackie-code@users.noreply.github.com>
2019-10-28 22:42:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+$("#js-shopping-list-form").submit(function(){
+    this.preventDefault)();
+    alert("handler called");
+    $("ul.shopping-list").append(
+        "<li>" +
+        $("#shopping-list-entry").val() + "</li>"
+    );
+});
+
+// "checks" the button by adding a class with .toggleClass that draws a line through it
+$(".shopping-item-toggle").click(function(){
+    $("button.shopping-item-toggle").toggleClass("m__checked");
+});
+
+// also could have done this and made the background color green instead of a line through the shoppint item
+// $(.shopping-item-toggle).click(function(){
+//     $(button.shopping-item-toggle).css("background-color", "green");
+// });
+
+
+// permanently removes items from the list
+$("button.shopping-item-delete").click(function(){
+    $("span.shopping-item").closest("li").remove();
+});
